3153b4715f69b943dc8da22b56fd2be17e5007cb
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-13 22:32:52 -0400

    1.4 Used ThreadPool for threads

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+
+            Employee employee = new Employee() { Name = "Oscar", CompanyName = "Owner" };
+            ThreadPool.QueueUserWorkItem(
+                new WaitCallback(DisplayEmployeeInfo), employee);
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayEmployeeInfo(object state)
+        {
+            Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+            Employee employee = (Employee)state;
+            Console.WriteLine($"Name: {employee.Name} and company name: {employee.CompanyName}");
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
