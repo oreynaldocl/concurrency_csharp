1a1b8af17b7a3f729df48d04eeff1c76ddbc779d
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-13 22:16:29 -0400

    1.3 Using local memory.

---
+++ LocalMemory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace LocalMemory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(Print1To100).Start();
+
+            Print1To100();
+        }
+
+        private static void Print1To100()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"{i + 1} ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
