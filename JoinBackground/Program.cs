0fa3cc2faaecd33fe63e7456953d25369c89e9bd
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-13 22:47:42 -0400

    1.6 Use Join for waiting and review IsBackground

---
+++ JoinBackground/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace JoinBackground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintHelloWorld);
+            thread.IsBackground = true; // Not sure what does
+            thread.Start();
+            thread.Join(); // makes the current thread wait until this is executed
+
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+
+        private static void PrintHelloWorld()
+        {
+            Console.WriteLine($"Hello WORLDDDDD!!!! {Thread.CurrentThread.IsBackground}");
+            Thread.Sleep(5000);
+        }
+    }
+}
