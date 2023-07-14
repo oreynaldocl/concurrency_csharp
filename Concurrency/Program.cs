497678f30a0c7c0972c077630af59047bbe8f93f
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-13 22:04:54 -0400

    1.1 Shared resource, bool have different value for each thread.

---
+++ Concurrency/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Concurrency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SharedResources/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SharedResources
+{
+    internal class Program
+    {
+        private static bool isCompleted = false;
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+            thread.Start();
+
+            // Main thread
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            if (!isCompleted)
+            {
+                // Log repeated twice because for each thread the `isCompleted` is false.
+                Console.WriteLine("Hello World should print only once.");
+                isCompleted = true;
+            }
+        }
+    }
+}
