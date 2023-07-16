1c866036c89d1d76f94b5ba6839abec9563ab9c3
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-16 17:07:13 -0400

    3.1 Used monitor.

---
+++ Synchronization/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchronization
+{
+    internal class Account
+    {
+        int balance;
+        object caztonLock = new object();
+        Random random = new Random(DateTime.UtcNow.Second);
+
+        public int Balance => balance;
+
+        public Account(int initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        int WithDraw(int amount)
+        {
+            if (balance == 0)
+            {
+                throw new Exception("Not enough balance1");
+            }
+            //lock (caztonLock) { } similar approach, but braces {} will describe the moment is unlock.
+
+            Monitor.Enter(caztonLock);
+            try
+            {
+                if (balance >= amount)
+                {
+                    Console.WriteLine($"Amount drawn: {amount}");
+                    balance -= amount;
+                    return balance;
+                }
+                else {
+                    Console.WriteLine($"Not possible to draw: {amount}");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(caztonLock);
+            }
+
+            return balance ;
+        }
+
+        public void WithdrawRandomly()
+        {
+            for (int i = 0; i < 10; i++) {
+                int toWithdraw = random.Next(100, 1000);
+                int balance = WithDraw(toWithdraw);
+                Console.WriteLine($"[{i}] Current balance {balance}");
+            }
+        }
+    }
+}
---
+++ Synchronization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Synchronization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UseMonitor();
+
+            Console.ReadKey();
+        }
+
+        static async void UseMonitor()
+        {
+            Account account = new Account(30000);
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"Starting {i} #################");
+                    account.WithdrawRandomly();
+                    Console.WriteLine($"Finished {i} #################");
+                });
+            }
+            Task.WaitAll(Task.FromResult(0));
+        }
+    }
+}
