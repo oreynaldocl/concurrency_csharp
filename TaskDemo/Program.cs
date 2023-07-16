852aa1fed45280bcc642d3315e9add8ba6344a4a
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-16 15:53:42 -0400

    2.5 Added TASK example

---
+++ TaskDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Review if we can join tasks in a thread
+            // One task that wait
+            Task<string> antecendt = Task.Run(() => DateTime.UtcNow.ToLongDateString());
+            // one task that calls to X function
+            Task<string> continuation = antecendt.ContinueWith(x => $"Today is {antecendt.Result}");
+            Console.WriteLine(continuation.Result);
+
+            Console.ReadKey();  
+        }
+    }
+}
