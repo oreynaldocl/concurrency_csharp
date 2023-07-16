a2a9c23e5b7838b5d4e8976f26a4d1c9b8c0223b
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-16 16:23:24 -0400

    2.6 Reviewing about order of logging tasks.

--- TaskDemo/Program.cs
+++ TaskDemo/Program.cs
@@ -9,12 +9,39 @@ namespace TaskDemo
         {
             // Review if we can join tasks in a thread
             // One task that wait
-            Task<string> antecendt = Task.Run(() => DateTime.UtcNow.ToLongDateString());
-            // one task that calls to X function
-            Task<string> continuation = antecendt.ContinueWith(x => $"Today is {antecendt.Result}");
-            Console.WriteLine(continuation.Result);
+            Task<string> antecendt = Task.Run(async () =>
+            {
+                Console.WriteLine($"{GetCurrentTime()} Building the required string");
+                await Task.Delay(2000);
+                return DateTime.UtcNow.ToString("MM/dd/yyyy hh:mm:ss.fff tt");
+            });
+            //delay.
+            var continuation = antecendt.ContinueWith((completed) =>
+            {
+                Console.WriteLine($"{GetCurrentTime()} Enter to continuation IsCompleted: {completed.IsCompleted}");
+                return $"Today is {completed.Result}";
+            });
+            Console.WriteLine($"{GetCurrentTime()} waiting for result {continuation.Result}");
+/*Result:
+08:20:25.254 Building the required string                               // (2)
+08:20:27.261 Enter to continuation IsCompleted: True                    // (3)
+08:20:25.222 waiting for result Today is 07/16/2023 08:20:27.261 PM     // (1)
+*/
 
-            Console.ReadKey();  
+            Console.ReadKey();
+        }
+        static string GetCurrentTime()
+        {
+            return DateTime.UtcNow.ToString("hh:mm:ss.fff");
         }
     }
 }
+/* TODO Needs to be reviewed if can join Delays with execution. And if delay can be aborted
+// one task that calls to X function
+var delay = Task.Delay(2500);
+var continuation = delay.ContinueWith((previous) => {
+    Console.WriteLine($"{GetCurrentTime()} After V1 wait {previous.IsCompleted}");
+    return antecendt.ContinueWith(x => $"Today is {x.Result}");
+});
+Console.WriteLine($"{GetCurrentTime()} waiting 2 results {continuation.Result.Result}");
+*/
