c670c33328e29733dd5e4106f0f733eb42a5b4a1
Oscar Callisaya <oreynaldo.cl@gmail.com>
2023-07-16 18:11:03 -0400

    3.5_2 Added mutex with wait period of time

--- Synchronization/MutexExample.cs
+++ Synchronization/MutexExample.cs
@@ -11,24 +11,35 @@ namespace Synchronization
         {
             for (int i = 0; i < 10; i++)
             {
-                Thread thread = new Thread(AcquireMutex);
+                Thread thread = new Thread(AcquireWaitMutex);
                 thread.Name = $"Thread{i}";
                 thread.Start();
             }
         }
 
-        private static void AcquireMutex()
+        private static void AcquireWaitMutex()
         {
-            mutex.WaitOne();
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false)) {
+                Console.WriteLine($"Mutex CAN'T BE ACQUIRE {Thread.CurrentThread.Name}");
+                return;
+            }
             DoSomething();
             mutex.ReleaseMutex();
-            Console.WriteLine($"Mutext releases by {Thread.CurrentThread.Name}");
+            Console.WriteLine($"Mutex releases by {Thread.CurrentThread.Name}");
+        }
+
+        private static void AcquireMutex()
+        {
+            mutex.WaitOne(); // Wait until the mutex is release
+            DoSomething();
+            mutex.ReleaseMutex();
+            Console.WriteLine($"Mutex releases by {Thread.CurrentThread.Name}");
         }
 
         private static void DoSomething()
         {
             Task.Delay(1000).Wait();
-            Console.WriteLine($"Mutext acquired by {Thread.CurrentThread.Name}");
+            Console.WriteLine($"Mutex acquired by {Thread.CurrentThread.Name}");
         }
     }
 }
